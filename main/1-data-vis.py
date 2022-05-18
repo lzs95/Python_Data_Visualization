87a63224497dd9180845bc41f636e92d07328e21
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-05-18 23:31:14 +0900

    feat: implement chart for users weekly

--- main/1-data-vis.py
+++ main/1-data-vis.py
@@ -5,8 +5,13 @@ from pytz import utc
 import matplotlib.pyplot as plt
 data = pandas.read_csv("./data/CC26Slack.csv" , parse_dates=["Date"])
 daily_members_average = data[["Date","Total enabled membership","Daily active members","Daily members posting messages","Messages posted"]]
+data["Weekday"] = data["Date"].dt.strftime("%A")
+data["Daynumber"] = data["Date"].dt.strftime("%w")
+weekday_avrg = data.groupby(["Weekday","Daynumber"]).mean()
+weekday_avrg = weekday_avrg.sort_values("Daynumber")
 
-chart = """{
+chart = """
+{
     title: {
         text: 'Slack Chat'
     },
@@ -80,72 +85,64 @@ chart = """{
   }
 }
 """
-chartMessaging = """
+
+chart_Users = """
 {
-  chart: {
-    type: 'bar'
-  },
-  title: {
-    text: 'Historic World Population by Region'
-  },
-  subtitle: {
-    text: ''
-  },
-  xAxis: {
-    categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
     title: {
-      text: null
-    }
-  },
-  yAxis: {
-    min: 0,
-    title: {
-      text: 'Population (millions)',
-      align: 'high'
+        text: 'Slack Chat'
     },
-    labels: {
-      overflow: 'justify'
-    }
-  },
-  tooltip: {
-    valueSuffix: ' millions'
-  },
-  plotOptions: {
-    bar: {
-      dataLabels: {
-        enabled: true
-      }
-    }
-  },
-  legend: {
+    subtitle: {
+        text: ''
+    },
+    xAxis: {
+        reversed: false,
+        title: {
+            enabled: true,
+            text: 'Date'
+        },
+        labels: {
+        format: '{value}'
+        },
+        maxPadding: 0.05,
+        showLastLabel: true
+    },
+    yAxis: {
+        title: {
+            text: 'Users'
+        },
+        labels: {
+        format: '{value}'
+        },
+        lineWidth: 2
+    },
+    legend: {
     layout: 'vertical',
     align: 'right',
-    verticalAlign: 'top',
-    x: -40,
-    y: 80,
-    floating: true,
-    borderWidth: 1,
-    backgroundColor:'#FFFFFF',
-    shadow: true
-  },
-  credits: {
-    enabled: false
-  },
-  series: [{
-    name: 'Year 1800',
-    data: [107, 31, 635, 203, 2]
-  }, {
-    name: 'Year 1900',
-    data: [133, 156, 947, 408, 6]
-  }, {
-    name: 'Year 2000',
-    data: [814, 841, 3714, 727, 31]
-  }, {
-    name: 'Year 2016',
-    data: [1216, 1001, 4436, 738, 40]
-  }]
+    verticalAlign: 'middle'
+    },
+    
+    series: [{
+        name: 'Users',
+        color: '#70ffb3',
+        data: [],
+    }],   
+    responsive: {
+    rules: [{
+      condition: {
+        maxWidth: 500
+      },
+      chartOptions: {
+        legend: {
+          layout: 'horizontal',
+          align: 'center',
+          verticalAlign: 'bottom'
+        }
+      }
+    }]
+  }
 }
 """
+
 
 def app():
     web_page = jp.QuasarPage()
@@ -159,9 +156,12 @@ def app():
     hc.options.series[0].data = list(daily_members_average["Daily active members"])
     hc.options.series[1].data = list(daily_members_average["Daily members posting messages"])
     
-    #Chart Messaging
-    h2 = jp.QDiv(a=web_page,text ="Messages", classes = "text-h2 text-center q-pa-md")
-    hcMsg = jp.HighCharts(a=web_page, options=chartMessaging)
+    #Average Users per Week
+    h2 = jp.QDiv(a=web_page,text ="Average Users Weekly", classes = "text-h3 text-center q-pa-md")
+    hc2 = jp.HighCharts(a=web_page, options=chart_Users)
+    hc2.options.xAxis.categories = list(weekday_avrg.index.get_level_values(0))
+    hc2.options.yAxis.categories = list(daily_members_average["Total enabled membership"])
+    hc2.options.series[0].data = list(weekday_avrg["Daily active members"])
     
     return web_page
 
@@ -170,11 +170,3 @@ def app():
 #Deploys webapp 
 jp.justpy(app)
 
-#  plotOptions: {
-#     series: {
-#       label: {
-#         connectorAllowed: false
-#       },
-#       pointStart: 2010
-#       }
-#     },
