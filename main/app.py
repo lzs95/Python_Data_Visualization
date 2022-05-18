1fd76a8b6ac03600e5c0e23c1eae4689fd9b6510
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-05-18 11:40:59 +0900

    feat: implement justpy main page

---
+++ main/app.py
@@ -0,0 +1,13 @@
+import justpy as jp
+
+def app():
+    web_page = jp.QuasarPage()
+    #Quasar Division
+    h2 = jp.QDiv(a=web_page,text ="CC26 SlackChat Analysis", classes = "text-h2 text-center q-pa-md")
+    p1 = jp.QDiv(a=web_page,text = "Active members")
+    return web_page
+
+
+
+#Deploys webapp 
+jp.justpy(app)
